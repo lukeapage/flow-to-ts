538ca4f22b2a24b7f707557c41004fcb0189bf0e
Luke Page <lukp@saxobank.com>
2020-11-18 13:28:24 +0100

    add testcase for more params

--- test/fixtures/convert/redux-select/create-selector/flow.js
+++ test/fixtures/convert/redux-select/create-selector/flow.js
@@ -3,3 +3,4 @@
 const a = createSelector<A, B, C, D>();
 const b = createSelector<A, void, C, D>();
 const c = createSelector<A, undefined, C, D>();
+const d = createSelector<A, B, C, D, E, F>();
--- test/fixtures/convert/redux-select/create-selector/ts.js
+++ test/fixtures/convert/redux-select/create-selector/ts.js
@@ -3,3 +3,4 @@
 const a = createSelector<A, B, D, C>();
 const b = createSelector<A, D, C>();
 const c = createSelector<A, D, C>();
+const d = createSelector<A, B, D, E, F, C>();
